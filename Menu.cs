cdbe1372adf80df4a58d9d2345ea4a8e0999e969
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-02 17:51:55 -0300

    Drawning scrren

--- Menu.cs
+++ Menu.cs
@@ -9,7 +9,33 @@ namespace HtlmEditor
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
-            Console.WriteLine("");
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            Header(50);
+            Body(50, 15);
+            Header(50);
+        }
+
+        public static void Header(int width)
+        {
+            Console.Write("+");
+            for (int i = 0; i <= width; i++) Console.Write("-");
+            Console.Write("+");
+        }
+
+        public static void Body(int width, int height)
+        {
+            Console.Write("\n");
+            for (int lines = 0; lines < height; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= width; i++) Console.Write(" ");
+                Console.Write("|");
+                Console.Write("\n");
+            }
         }
     }
 }
